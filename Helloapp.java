d1fb728ef7737ee0528fe3ad5ff5c6ab293ffd3c
muvvavamsi97-svg <muvvavamsi97@gmail.com>
2026-04-06 23:56:53 +0530

    Add files via upload

---
+++ Helloapp.java
@@ -0,0 +1,11 @@
+/*
+ * HelloApp - UC1 - Display Hello World
+ * @author Adit
+ * @version 1.0
+ */
+public class Helloapp {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        
+    }
+}
